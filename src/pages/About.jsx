ebf977e18587e971ca323ea35b08c1bbe13194ac
Dylan Casey <dylan.casey.john@gmail.com>
2025-06-17 18:20:29 -0400

    Updated index.css file so that overflow-x is hidden to prevent content from moving left in mobile view

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 export default function About() {
-    return (
-      <div className="text-white text-2xl text-center mt-20">
-        About Crush Guitars
+  return (
+     <div className="text-white text-2xl text-center mt-20">
+        About
       </div>
     );
   }
